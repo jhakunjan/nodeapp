b85d1c27b849b1e4d96f00d2c94476d2cb3f8649
Kunjan Jha <kunjan.jha@nycha.nyc.gov>
2025-04-22 21:13:02 +0530

    add jenkinsfile

---
+++ manage-vm/manage-vm.ps1
@@ -0,0 +1,29 @@
+param (
+    [Parameter(Mandatory=$true)][string]$action,
+    [Parameter(Mandatory=$true)][string]$vmName,
+    [Parameter(Mandatory=$true)][string]$resourceGroup
+)
+
+$ErrorActionPreference = "Stop"
+$ConfirmPreference = "None"
+
+Write-Host "🔁 Action: $action on VM: $vmName in RG: $resourceGroup"
+
+switch ($action.ToLower()) {
+    "start" {
+        Start-AzVM -Name $vmName -ResourceGroupName $resourceGroup
+        Write-Host "✅ VM '$vmName' started."
+    }
+    "stop" {
+        Stop-AzVM -Name $vmName -ResourceGroupName $resourceGroup -Force
+        Write-Host "🛑 VM '$vmName' stopped."
+    }
+    "status" {
+        $vm = Get-AzVM -Name $vmName -ResourceGroupName $resourceGroup -Status
+        $status = $vm.Statuses | Where-Object { $_.Code -like 'PowerState/*' }
+        Write-Host "📡 Status: $($status.DisplayStatus)"
+    }
+    default {
+        throw "❌ Invalid action: $action"
+    }
+}
